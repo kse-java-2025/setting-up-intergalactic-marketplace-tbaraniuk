c46700d215163f2bd2e1e930ebe035c8ea1509c6
tbaraniuk <baraniuktaras098@gmail.com>
2025-12-12 10:45:21 +0200

    fixed files

--- src/main/java/com/example/intergalactic_marketplace/config/security/SecurityConfiguration.java
+++ src/main/java/com/example/intergalactic_marketplace/config/security/SecurityConfiguration.java
@@ -3,10 +3,12 @@ package com.example.intergalactic_marketplace.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,6 +19,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
     private static final String API_V1_PRODUCTS = "/api/v1/products/**";
+    private static final String API_V1_GREETINGS = "/api/v1/greetings/**";
     
     @Bean
     @Profile("test")
@@ -33,11 +36,13 @@ public class SecurityConfiguration {
     }
     
     @Bean
+    @Order(1)
     @Profile("!test")
     public SecurityFilterChain filterChainProducts(HttpSecurity http) throws Exception {
         http
                 .securityMatcher(API_V1_PRODUCTS)
                 .cors(withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers(HttpMethod.GET, API_V1_PRODUCTS).permitAll()
@@ -49,8 +54,16 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain filterChainAll(HttpSecurity http) throws Exception {
-        http.cors(withDefaults());
+    @Order(2)
+    @Profile("!test")
+    public SecurityFilterChain filterChainGreetings(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher(API_V1_GREETINGS)
+                .cors(withDefaults())
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable());
 
         return http.build();
     }
--- src/main/java/com/example/intergalactic_marketplace/util/SecurityUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.intergalactic_marketplace.util;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class SecurityUtil {
-    public static final String API_KEY_HEADER = "X-Cosmic-Marketplace-Api-Key";
-    public static final String ROLE_CLAIMS_HEADER = "X-Roles-Claims";
-}
